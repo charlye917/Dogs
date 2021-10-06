076bdb59d6248e23a4a340b959f694e40ac22061
charlye934 <charlye934@gmail.com>
2021-10-05 22:36:25 -0500

    se agregaron configuracion notificacion

---
+++ app/src/main/java/com/charlye934/dogs/util/NotificationHelper.kt
@@ -0,0 +1,55 @@
+package com.charlye934.dogs.util
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+import com.charlye934.dogs.MainActivity
+import com.charlye934.dogs.R
+
+class NotificationHelper(private val context: Context) {
+
+    private val CHANNEL_ID = "Dogs channerl id"
+    private val NOTIFICATION_ID = 123
+
+    private val icon = BitmapFactory.decodeResource(context.resources, R.drawable.ic_dog)
+    private val intent = Intent(context, MainActivity::class.java).apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+    }
+    private val pendingIntent = PendingIntent.getActivity(context, 0, intent, 0)
+
+    fun createNotification(){
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_dog)
+            .setContentTitle("Dogs retrived")
+            .setContentText("This notification has some content")
+            .setStyle(NotificationCompat.BigPictureStyle()
+                .bigPicture(icon)
+                .bigLargeIcon(null))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        NotificationManagerCompat.from(context).notify(NOTIFICATION_ID, builder)
+    }
+
+    private fun createNotificationChannel(){
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+            val name = CHANNEL_ID
+            val descriptionText = "Chanel description"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+}
